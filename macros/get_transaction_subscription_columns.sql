a6c2d6dd5f7d73d7e831a27ce7b3b3c0062ff0d7
Dimosthenis Schizas <dimos@hackthebox.eu>
2022-03-08 21:20:09 +0200

    Add initial macros

---
+++ macros/get_transaction_subscription_columns.sql
@@ -0,0 +1,11 @@
+{% macro get_transaction_subscription_columns() %}
+
+{% set columns = [
+    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
+    {"name": "transaction_id", "datatype": dbt_utils.type_string()},
+    {"name": "subscription_id", "datatype": dbt_utils.type_string()},
+] %}
+
+{{ return(columns) }}
+
+{% endmacro %}
